23a6859010e523eca2aca2a8fac628b2a8eb9bf9
Stillman <ns115@ic.ac.uk>
2018-04-19 14:18:49 +0100

    Added code to put date column into registration data.

--- R/clean-reg-data.R
+++ R/clean-reg-data.R
@@ -1,3 +1,12 @@
+library(stringr)
+
 # Add cleventdate column to REG_data
 # 
-# --- date column adding code here ---
+fileNames <- Sys.glob("data-raw/REG_*.xlsx")
+fileName_date_pattern <- "_([0-9]*)\\.xlsx"
+event_dates <- str_match(fileNames, fileName_date_pattern)[,2]
+dt_format <- "%d%m%y"
+
+event_dates <- as.Date(event_dates, format = dt_format)
+
+REG_data$cleventdate <- event_dates[1]
