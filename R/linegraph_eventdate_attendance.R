7265449d23c1d08f050efa8b8faf78a11bcaead7
Lovett <dlovett@ic.ac.uk>
2018-04-24 15:57:03 +0100

    Code to plot line graph of number of attendances for each CL event

---
+++ R/linegraph_eventdate_attendance.R
@@ -0,0 +1,13 @@
+#Create line graph of number of attendances for each event
+#Code requires tidyverse package
+#as.character was used to restrict x axis to not show months between event dates
+#Removing as.character means x axis includes months between event dates
+#This code requires REG_data to have "cleventdate" variable
+#Registration counts can be clarified using this code: "table(REG_data$cleventdate)"
+
+library("tidyverse")
+ggplot(REG_data,aes( x = as.character(cleventdate), group = 1))
+  + geom_line(stat = "count")
+  + xlab("CL Event Date")
+  + ylab("Number of attendees")
+  + ggtitle("Line graph of number of attendances for each CL Event")
