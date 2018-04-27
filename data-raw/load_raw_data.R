74b6acaa820feb5eff706021c490bb5a4b7f31e3
Adeleke <ya213@ic.ac.uk>
2018-04-27 16:15:48 +0100

    Commented out packages needed to run this code

--- data-raw/load_raw_data.R
+++ data-raw/load_raw_data.R
@@ -4,25 +4,25 @@
 # All such files will have a name beginning with "REG_Report_"
 # Relevant data is contained in the "Delegate_Details" tab
 
+#you will need the following packages installed for the code below
 #install tidyverse
-install.packages("tidyverse")
+#install.packages("tidyverse")
+#install readxl package
+#install.packages("readxl")
 
 #Open tidyverse
 library(tidyverse)
 
-
 #Identify all fileneames starting with "REG_Report_*.xls) in data-raw folder
 filenames<-Sys.glob("data-raw/REG_Report_*.xlsx")
-
-#install readxl package
-install.packages("readxl")
 
 #open "readxl" to use "read_excel" function
 library(readxl)
 
 #create load_REG_files function to load files into R (fn=filenames, fne=fnelement)
-load_REG_files<-function(fn){lapply(fn,function(fne){load_files<-read_excel(fne,sheet="Delegate_Details",skip=1)})}
-
+load_REG_files<-function(fn)
+{lapply(fn,function(fne)
+{read_excel(fne,sheet="Delegate_Details",skip=1)})}
 
 #apply function "load_REG-files" to values "filenames" and assigned to "data_frame"
 data_frames<-load_REG_files(filenames)
