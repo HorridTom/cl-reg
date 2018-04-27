083cab7ba7be420d811a2e5e8c050b56acc792b2
Ryan <pryan@ic.ac.uk>
2018-04-27 16:33:57 +0100

    Fixed variable descriptions

--- R/data.R
+++ R/data.R
@@ -7,23 +7,23 @@
 #'   \item{firstname}{first name of the registrant, chr}
 #'   \item{lastname}{last name of registrant, chr}
 #'   \item{nhs provider: acute services}{organisation of registrant: which NWL acute service provider, factor}
-#'   \item{nhs provider: community}{organisation of registrant: which NWL community provider, chr}
-#'   \item{nhs provider: mental health}{organisation of registrant: which NWL mental health provider, chr}
-#'   \item{nhs commissioners}{organisation of registrant: which NWL NHS commissioner, chr}
-#'   \item{academic networks}{organisation of registrant: which NWL academic network, chr}
-#'   \item{academic institutions}{organisation of registrant: which NWL academic institution, chr}
-#'   \item{social services}{organisation of registrant: which social service, chr}
-#'   \item{third sector}{organisation of registrant: which third sector, chr}
-#'   \item{industry}{organisation of registrant: which industry, chr}
-#'   \item{other or none}{organisation of registrant: other or none, factor}
+#'   \item{nhs provider: community}{organisation of registrant: which NWL community provider, factor}
+#'   \item{nhs provider: mental health}{organisation of registrant: which NWL mental health provider, factor}
+#'   \item{nhs commissioners}{organisation of registrant: which NWL NHS commissioner, factor}
+#'   \item{academic networks}{organisation of registrant: which NWL academic network, factor}
+#'   \item{academic institutions}{organisation of registrant: which NWL academic institution, factor}
+#'   \item{social services}{organisation of registrant: which social service, factor}
+#'   \item{third sector}{organisation of registrant: which third sector, factor}
+#'   \item{industry}{organisation of registrant: which industry, factor}
+#'   \item{other or none}{organisation of registrant: other or none, chr}
 #'   \item{nihr attendee}{organisation of registrant: which NIHR organisation, chr}
-#'   \item{project member}{CLAHRC NWL project member: which team, chr}
-#'   \item{patient representative}{is registrant a patient representative, chr}
-#'   \item{partner organisation}{is registrant a member of a partner organisation, chr}
-#'   \item{industry__1}{organisation of registrant: industry company name, chr}
-#'   \item{other selection}{organisation of registrant: other selection, factor}
-#'   \item{if other please specify}{organisation of registrant: other specified, factor}
-#'   \item{clahrc delivery themes proposed workstream}{description, factor}
+#'   \item{project member}{CLAHRC NWL project member: which team, factor}
+#'   \item{patient representative}{is registrant a patient representative, factor}
+#'   \item{partner organisation}{is registrant a member of a partner organisation, factor}
+#'   \item{industry__1}{organisation of registrant: industry company name, factor}
+#'   \item{other selection}{organisation of registrant: other selection, chr}
+#'   \item{if other please specify}{organisation of registrant: other specified, chr}
+#'   \item{clahrc delivery themes proposed workstream}{description, chr}
 #' }
 #'
 #'
