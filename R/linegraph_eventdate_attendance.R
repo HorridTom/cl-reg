b8d7d696c97cf71e92dbd2eb5a196daac6c33cd5
Woodcock <tw299@ic.ac.uk>
2018-05-01 12:41:54 +0100

    Changed ggplot data argument to df

--- R/linegraph_eventdate_attendance.R
+++ R/linegraph_eventdate_attendance.R
@@ -9,7 +9,7 @@ library("tidyverse")
 # df= dataframe containing the data to be plotted
 # event_date = column containing number of attendances for each event
 plot_reg_no <- function(df) {
-  reg_no <- ggplot(REG_data,aes( x = as.character(cleventdate), group = 1)) + 
+  reg_no <- ggplot(df, aes( x = as.character(cleventdate), group = 1)) + 
   geom_line(stat = "count") + 
   xlab("CL Event Date") + 
   ylab("Number of attendees") + 
